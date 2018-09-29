0ed5dc3f03d23a8296ae7f59371c8dc9e405f930
Sandy Fogarassy <sandy.fogarassy@mail.utoronto.ca>
2018-09-29 15:31:58 -0400

    adding Mccdaq cpp code

---
+++ ground-station/DAQ/Mccdaq.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include "Mccdaq.h"
+
+class Daq {
+	//Parent class for the entire program
+	public: 
+
+		
+
+	private: 
+	protected: 
+}
+
+
+int main() {
+	const char* a = "hello world";
+	printf("%s \n", a);
+	return 0;
+}
---
+++ ground-station/DAQ/Mccdaq.h
@@ -0,0 +1,11 @@
+#ifndef MCCDAQ_H
+#define MCCDAQ_H
+
+#include <C:\Users\Public\Documents\measurement_computing\DAQ\C\cbw.h>
+#include <C:\Users\Public\Documents\measurement_computing\DAQ\C\cbw32.lib>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
