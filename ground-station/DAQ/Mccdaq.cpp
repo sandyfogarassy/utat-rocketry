a8ce2f48a8a4f9c6dad3e7e5a050f52fc8ae43f1
Sandy Fogarassy <sandy.fogarassy@mail.utoronto.ca>
2018-10-04 12:18:34 -0400

    adding JSON to daq files

--- ground-station/DAQ/Mccdaq.cpp
+++ ground-station/DAQ/Mccdaq.cpp
@@ -2,19 +2,10 @@
 #include <iostream>
 #include "Mccdaq.h"
 
-class Daq {
-	//Parent class for the entire program
-	public: 
-
-		
-
-	private: 
-	protected: 
-}
-
-
 int main() {
+
 	const char* a = "hello world";
 	printf("%s \n", a);
 	return 0;
+	
 }
--- ground-station/DAQ/Mccdaq.h
+++ ground-station/DAQ/Mccdaq.h
@@ -3,9 +3,50 @@
 
 #include <C:\Users\Public\Documents\measurement_computing\DAQ\C\cbw.h>
 #include <C:\Users\Public\Documents\measurement_computing\DAQ\C\cbw32.lib>
+#include <json/writer.h>
+#include <iostream>
+#include <fstream> 
+#include <json.h> 
+
+class Daq {
+	//Parent class for the entire program
+	public: 
+
+		
+
+	private: 
+	protected: 
+}
 
 using namespace std;
 
-int main() {
-    return 0;
+void save_file() {
+
+}
+
+void write_to_JSON() {
+
+    Json::Value event;
+    Json::Value vec(Json::arrayValue);
+
+    //Putting DAQ feedback into JSON objects
+    event["DAQ"]["PT"]["code"] = "Ox. Temp 1";
+    event["DAQ"]["PT"]["value"] = "600psi";
+
+    //Putting arduino feedback into JSON objects
+    event["arduino"]["Load Cell"]["code"] = "Thrust Load Cell";
+    event["arduino"]["Load Cell"]["value"] = "2000N";
+
+    //Writing data to JSON file
+    std::ofstream file;
+    file.open("data_00001.json");
+    Json::StyledWriter styledWriter;
+    file << styledWriter.write(event);
+    file.close();
+
+}
+
+void main() {
+
+
 }
