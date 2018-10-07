a1e3f9d565f889cb176e4008ecfa213c7742a2cd
Sandy Fogarassy <sandy.fogarassy@mail.utoronto.ca>
2018-10-07 16:37:38 -0400

    added c++ writing to JSON files in JSON format

--- ground-station/DAQ/Mccdaq.cpp
+++ ground-station/DAQ/Mccdaq.cpp
@@ -1,11 +1,42 @@
 #include <stdio.h>
 #include <iostream>
-#include "Mccdaq.h"
+#include <fstream>
+#include <iomanip>
+#include <C:\Users\sandy\OneDrive\Desktop\utat-rocketry\ground-station\DAQ\jsoncpp\dist\jsoncpp.cpp>
+#include <C:\Users\sandy\OneDrive\Desktop\utat-rocketry\ground-station\DAQ\jsoncpp\dist\json\json.h>
+#include <C:\Users\sandy\OneDrive\Desktop\utat-rocketry\ground-station\DAQ\jsoncpp\dist\json\json-forwards.h>
+
+//#include "Mccdaq.h"
+
+void write_to_JSON() {
+
+	//initializing overarching JSON object
+	Json::Value data;   
+
+	//initializing JSON vector
+    Json::Value vec(Json::arrayValue);
+
+	//testing output of vector
+    vec.append(Json::Value(1));
+    vec.append(Json::Value(2));
+    vec.append(Json::Value(3));
+
+	//JSON will have two branches underneath data, one for the DAQ, and one for the arduino
+    data["DAQ"]["PT"] = "test";
+	data["DAQ"]["Load Cell"] = "test";
+    data["Arduino"]["Thermocouple"] = "test";
+    data["Arduino"]["Load Cell"]=vec;
+
+    std::cout << "SUCCESS" << std::endl;
+
+	std::ofstream ofs ("test.json", std::ofstream::app);
+    ofs << data << std::flush;
+
+}
 
 int main() {
 
-	const char* a = "hello world";
-	printf("%s \n", a);
+	write_to_JSON();
 	return 0;
 	
 }
--- ground-station/DAQ/Mccdaq.h
+++ ground-station/DAQ/Mccdaq.h
@@ -1,4 +1,4 @@
-#ifndef MCCDAQ_H
+/*#ifndef MCCDAQ_H
 #define MCCDAQ_H
 
 #include <C:\Users\Public\Documents\measurement_computing\DAQ\C\cbw.h>
@@ -12,41 +12,11 @@ class Daq {
 	//Parent class for the entire program
 	public: 
 
-		
-
+        void save_file();
+        void write_to_JSON();
+	
 	private: 
 	protected: 
-}
-
-using namespace std;
-
-void save_file() {
-
-}
-
-void write_to_JSON() {
-
-    Json::Value event;
-    Json::Value vec(Json::arrayValue);
-
-    //Putting DAQ feedback into JSON objects
-    event["DAQ"]["PT"]["code"] = "Ox. Temp 1";
-    event["DAQ"]["PT"]["value"] = "600psi";
-
-    //Putting arduino feedback into JSON objects
-    event["arduino"]["Load Cell"]["code"] = "Thrust Load Cell";
-    event["arduino"]["Load Cell"]["value"] = "2000N";
-
-    //Writing data to JSON file
-    std::ofstream file;
-    file.open("data_00001.json");
-    Json::StyledWriter styledWriter;
-    file << styledWriter.write(event);
-    file.close();
-
-}
-
-void main() {
+}*/
 
 
-}
