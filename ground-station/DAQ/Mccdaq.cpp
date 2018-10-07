4cecdee9fa3359b6d4ee2be9d8cd52b4b08b2535
Sandy Fogarassy <sandy.fogarassy@mail.utoronto.ca>
2018-10-07 19:40:44 -0400

    now writes values to JSON file

--- ground-station/DAQ/Mccdaq.cpp
+++ ground-station/DAQ/Mccdaq.cpp
@@ -13,19 +13,76 @@ void write_to_JSON() {
 	//initializing overarching JSON object
 	Json::Value data;   
 
-	//initializing JSON vector
-    Json::Value vec(Json::arrayValue);
+	//initializing JSON vectors
+    Json::Value tc1(Json::arrayValue);
+	Json::Value tc2(Json::arrayValue);
+	Json::Value tc3(Json::arrayValue);
+	Json::Value tc4(Json::arrayValue);
 
-	//testing output of vector
-    vec.append(Json::Value(1));
-    vec.append(Json::Value(2));
-    vec.append(Json::Value(3));
+	Json::Value pt1(Json::arrayValue);
+	Json::Value pt2(Json::arrayValue);
+	Json::Value pt3(Json::arrayValue);
+
+	//testing output of vectors
+    tc1.append(Json::Value(52));
+    tc1.append(Json::Value(55));
+    tc1.append(Json::Value(50));
+	tc1.append(Json::Value(51));
+    tc1.append(Json::Value(51));
+    tc1.append(Json::Value(50));
+
+	tc2.append(Json::Value(49));
+    tc2.append(Json::Value(50));
+    tc2.append(Json::Value(49));
+	tc2.append(Json::Value(49));
+    tc2.append(Json::Value(49));
+    tc2.append(Json::Value(49));
+
+	tc3.append(Json::Value(48));
+    tc3.append(Json::Value(50));
+    tc3.append(Json::Value(51));
+	tc3.append(Json::Value(51));
+    tc3.append(Json::Value(51));
+    tc3.append(Json::Value(50));
+
+	tc4.append(Json::Value(52));
+    tc4.append(Json::Value(50));
+    tc4.append(Json::Value(51));
+	tc4.append(Json::Value(51));
+    tc4.append(Json::Value(51));
+    tc4.append(Json::Value(50));
+
+	pt1.append(Json::Value(600));
+    pt1.append(Json::Value(611));
+    pt1.append(Json::Value(602));
+	pt1.append(Json::Value(604));
+    pt1.append(Json::Value(605));
+    pt1.append(Json::Value(606));
+
+	pt2.append(Json::Value(600));
+    pt2.append(Json::Value(609));
+    pt2.append(Json::Value(600));
+	pt2.append(Json::Value(602));
+    pt2.append(Json::Value(603));
+    pt2.append(Json::Value(601));
+
+	pt3.append(Json::Value(599));
+    pt3.append(Json::Value(602));
+    pt3.append(Json::Value(602));
+	pt3.append(Json::Value(604));
+    pt3.append(Json::Value(605));
+    pt3.append(Json::Value(601));
 
 	//JSON will have two branches underneath data, one for the DAQ, and one for the arduino
-    data["DAQ"]["PT"] = "test";
+    data["DAQ"]["PT1"] = pt1;
+	data["DAQ"]["PT2"] = pt2;
+	data["DAQ"]["PT3"] = pt3;
 	data["DAQ"]["Load Cell"] = "test";
-    data["Arduino"]["Thermocouple"] = "test";
-    data["Arduino"]["Load Cell"]=vec;
+    data["Arduino"]["Thermocouple1"] = tc1;
+	data["Arduino"]["Thermocouple2"] = tc2;
+	data["Arduino"]["Thermocouple3"] = tc3;
+	data["Arduino"]["Thermocouple4"] = tc4;
+    data["Arduino"]["Load Cell"] = "test";
 
     std::cout << "SUCCESS" << std::endl;
 
